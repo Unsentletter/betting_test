ec6c414c749df2a510afd13913aed037c5e480a7
Tristan Southwell <tristan.southwell@hotmail.com>
2017-07-29 10:46:19 +1000

    running out of time

--- standard.js
+++ standard.js
@@ -2,27 +2,43 @@ const fs = require('fs'), filename = 'bets.txt';
 const data = fs.readFileSync("bets.txt").toString('utf-8');
 let text = data.split(/[ ]+/);
 
+// console.log(text);
+
+
 const winComission = 0.15;
 const placeComission = 0.12;
 const exactaComission = 0.18;
-let winnerPool, placePool, exactaPool, result;
+let winnerPool, placePool, exactaPool, result, first, second, third;
+let winningBets = {};
 let winNum = 0;
 let placeNum = 0;
 let exactaNum = 0;
 
 
 
-for(let i = 0; i < text.length; i++){
+for(let i = 0; i < text.length; i++) {
   if (text[i].includes('Result')) {
 
-    // console.log(text[i])
+    // console.log('result', text[i].charAt(7))
 
+    first = text[i].charAt(7);
+    second = text[i].charAt(9);
+    third = text[i].charAt(11);
 
-  } else if (text[i].includes('W')) {
+  }
+}
+for(let i = 0; i < text.length; i++) {
+  if (text[i].includes('W')) {
     let winners = text[i].split(/[:]+/);
+
 
     winnerPool = winNum += parseInt(winners[winners.length - 1])
 
+console.log(text[i].charAt(6));
+
+    // if (text[i].charAt(6) === first) {
+    //   winningBets.push('test')
+    // }
 
   } else if (text[i].includes('P')) {
     // let placers = text[i].split(/[:]+/);
@@ -41,13 +57,18 @@ for(let i = 0; i < text.length; i++){
   }
 }
 
-function takeComissionWin (pool, comission) {
+function takeComissionWin(pool, comission) {
   return pool * comission;
 }
 
-console.log(takeComissionWin(winnerPool, winComission));
+function addPayoutPool() {
+
+}
+
+console.log('winners pool commission', takeComissionWin(winnerPool, winComission));
 
 
-console.log(winnerPool);
+console.log('winners pool', third);
 // console.log(placePool);
 // console.log(exactaPool);
+console.log(winningBets);
